b6266565d2ca068cc46db31b9ff31108d43e0e00
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-08-02 18:59:11 +0300

    Added Homework 7: index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict"
+// 1
+function sumTo(n){
+    if (n==0) {
+        return 1;
+    } else {
+        if (n==1){
+        return n;
+        } else {
+        return n + sumTo(n-1);
+        }
+    }
+}
+console.log(sumTo(4)); // 10
+console.log(sumTo(3)); // 6
+
+// 2
+// function findShort(sentence){
+//     let arr = sentence.split(' ');
+//     let sorted = arr.sort((a, b) => a.length - b.length);
+//     return sorted[0];
+// }
+
+// const sentence = "Lorem ipsum dolor sit amet";
+// console.log(findShort(sentence)); // sit
+// console.log(findShort("Hello world")); // Hello
+// console.log(findShort("Hi")); // Hi
+// console.log(findShort("She is David's sister")); // is
